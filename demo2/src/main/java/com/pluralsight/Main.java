b60069a53c0307d3e9c81983bbc578aa842dabe3
Jevaughn Fowler <jevaughnfowler@gmail.com>
2025-04-11 10:24:20 -0400

    add demo3 project

--- demo2/src/main/java/com/pluralsight/Main.java
+++ demo2/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,10 @@
 package com.pluralsight;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         System.out.println("Hello, World! from demo2");
+
     }
 }
